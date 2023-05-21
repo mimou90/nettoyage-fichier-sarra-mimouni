257cbf3b0a117d32462f0c218d89aedfb32071c7
sarra Mimouni <sarra.mimouni@hotmail.fr>
2023-05-21 13:07:42 +0200

    modif fichier

--- result_csv.py
+++ result_csv.py
@@ -4,4 +4,4 @@ filtres = ["Credit", "Debit","Service"]
 df_trie = df [df["Series_titles_2"].isin(filtres)]
 print(df_trie)
 df['ID'] = range (1, len(df) + 1)
-df_trie.to_csv("result.csv", index=False)
+df_trie.to_csv("C:/Users/sarra/Desktop/dc4 git/nettoyage-fichier-sarra-mimouni/result.csv", index=False)
