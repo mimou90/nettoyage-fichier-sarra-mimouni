4e359475421ae8916031057bbae18cca540be2f6
sarra Mimouni <sarra.mimouni@hotmail.fr>
2023-05-21 13:02:50 +0200

    nouvelles données

---
+++ result_csv.py
@@ -0,0 +1,7 @@
+import pandas as pd
+df = pd.read_csv("C:/Users/sarra/Desktop/dc4 git/nettoyage-fichier-sarra-mimouni/test.csv")
+filtres = ["Credit", "Debit","Service"]
+df_trie = df [df["Series_titles_2"].isin(filtres)]
+print(df_trie)
+df['ID'] = range (1, len(df) + 1)
+df_trie.to_csv("result.csv", index=False)
