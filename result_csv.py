66c41bdcb23e410734e156574a13268a7fda5ea7
sarra Mimouni <sarra.mimouni@hotmail.fr>
2023-05-21 13:49:27 +0200

    correction code

--- result_csv.py
+++ result_csv.py
@@ -1,12 +1,11 @@
 import pandas as pd
 df = pd.read_csv("C:/Users/sarra/Desktop/dc4 git/nettoyage-fichier-sarra-mimouni/test.csv")
 filtres = ["Credit", "Debit","Service"]
-df_trie = df [df["Series_titles_2"].isin(filtres)]
+df_trie = df [df["Series_title_2"].isin(filtres)]
 print(df_trie)
 df['ID'] = range (1, len(df) + 1)
 
 print (df_trie)
-for index, row in df_trie.iterrows():
-    print(row)
+
 
 df_trie.to_csv("C:/Users/sarra/Desktop/dc4 git/nettoyage-fichier-sarra-mimouni/result.csv", index=False)
