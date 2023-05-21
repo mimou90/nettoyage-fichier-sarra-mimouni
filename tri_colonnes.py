f53eae63f873136c24eb4eca8f6d4afb08ad92de
sarra Mimouni <sarra.mimouni@hotmail.fr>
2023-05-21 13:40:22 +0200

    modif code

--- tri_colonnes.py
+++ tri_colonnes.py
@@ -1,4 +1,4 @@
 import pandas as pd
 df = pd.read_csv("C:/Users/sarra/Desktop/dc4 git/nettoyage-fichier-sarra-mimouni/test.csv")
 df = df.drop(columns=['Series_reference','Suppressed','STATUS','UNITS','Subject','Group','Series_title_1','Series_title_3','Series_title_4','Series_title_5'])
-df.to_csv ("C:/Users/sarra/Desktop/dc4 git/nettoyage-fichier-sarra-mimouni/test_sortie.csv", index=False)
+df.to_csv ("C:/Users/sarra/Desktop/dc4 git/nettoyage-fichier-sarra-mimouni/result.csv", index=False)
